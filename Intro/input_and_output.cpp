d12d67e3bc1a894e374c59c035d52cf5e1bde65c
Salman-H <sah517@g.harvard.edu>
2019-11-01 00:17:05 -0400

    input and output v1

---
+++ Intro/input_and_output.cpp
@@ -0,0 +1,45 @@
+/*
+Input and Output
+
+Task
+-----
+Read 3 numbers from stdin and print their sum to stdout.
+
+Note: If you plan on completing this challenge in C instead of C++, you'll
+need to use format specifiers with printf and scanf.
+
+Input Format
+------------
+A single line containing 3 space-separated integers: a, b, and c.
+
+Constraints
+-----------
+1 <= a,b,c <= 1000
+
+Output Format
+-------------
+Print the sum of the three numbers on a single line.
+
+Sample Input
+------------
+1 2 7
+
+Sample Output
+-------------
+10
+*/
+
+#include <iostream>
+
+int main() 
+{
+  int sum{};
+  for (int i{}; i<3; ++i)
+  {
+    int num{};
+    std::cin >> num;
+    sum += num;
+  }
+  std::cout << sum;
+  return 0;
+}
