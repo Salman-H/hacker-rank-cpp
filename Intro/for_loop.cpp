eb61f14d9fdc34fc0c33549aea50a12d6fe15f22
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2020-01-02 16:42:37 -0500

    for loop v2

--- Intro/for_loop.cpp
+++ Intro/for_loop.cpp
@@ -35,23 +35,13 @@ int main()
   int a{};
   int b{};
   std::cin >> a >> b;
-
+  std::string digits[9] = { "one", "two", "three", "four",
+                            "five", "six", "seven", "eight", "nine"};
   for (int i{a}; i<=b; ++i)
   {
     if (i >= 1 && i <= 9)
     {
-      switch(i)
-      {
-        case 1:  std::cout << "one"   << '\n';  break;
-        case 2:  std::cout << "two"   << '\n';  break;
-        case 3:  std::cout << "three" << '\n';  break;
-        case 4:  std::cout << "four"  << '\n';  break;
-        case 5:  std::cout << "five"  << '\n';  break;
-        case 6:  std::cout << "six"   << '\n';  break;
-        case 7:  std::cout << "seven" << '\n';  break;
-        case 8:  std::cout << "eight" << '\n';  break;
-        case 9:  std::cout << "nine"  << '\n';  break;
-      }
+      std::cout << digits[i-1] << '\n';
     }
     else if (i > 9)
     {
@@ -59,6 +49,5 @@ int main()
       else std::cout << "odd" << '\n';
     }
   }
-
   return 0;
 }
